403e6dd8484b6f1406042e580129a13180ff52a6
1F10220253 <s1f102202538@iniad.org>
2023-08-30 15:44:44 +0900

    setting model

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    date1 = models.DateTimeField()
+    date2 = models.DateTimeField()
+    date3 = models.DateTimeField()
+    money = models.IntegerField(blank=True, default=0)
